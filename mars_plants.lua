8a3f09a17da6ffd8e543f3c42367926522d050b7
Lanhild <73610779+Lanhild@users.noreply.github.com>
2021-02-02 20:24:30 -0500

    corrects a crash issue

--- mars_plants.lua
+++ mars_plants.lua
@@ -110,7 +110,7 @@ for i=2,5 do
 	})
 end
 minetest.register_node(":mars:red_dirt_with_grass", {
-	description = S("Red Dirt"),
+	description = ("Red Dirt"),
 	tiles = {"red_grass.png", "red_dirt.png",
 		{name = "red_dirt.png^red_grass_side.png",
 			tileable_vertical = false}},
