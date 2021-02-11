bbe75aa82da713d66a155e2a98c68c5717536459
Oversword <bionic@oversword.co.uk>
2021-02-11 22:02:52 +0000

    Remove stars from the skybox above 5000 (#4)
    * Remove stars from the skybox above 5000
    
    * Switch it back on in normal earth
    
    Co-authored-by: Oversword <camabyss@gmail.com>

--- skybox.lua
+++ skybox.lua
@@ -65,6 +65,7 @@ minetest.register_globalstep(function(dtime)
             player:set_sky(normal_sky)
             player:set_moon({visible = true})
             player:set_sun({visible = true})
+            player:set_stars({visible = true})
 			player_list[name] = "earth"
 			if otherworlds.settings.gravity.enable then
 				player:set_physics_override({gravity = 1})
@@ -81,6 +82,7 @@ minetest.register_globalstep(function(dtime)
             })
             player:set_sun({visible = false, sunrise_visible = false})
             player:set_moon({visible = false})
+            player:set_stars({visible = false})
 			player_list[name] = "space"
 			if otherworlds.settings.gravity.enable then
 				player:set_physics_override({gravity = 0.4})
@@ -97,6 +99,7 @@ minetest.register_globalstep(function(dtime)
             })
             player:set_sun({visible = false, sunrise_visible = false})
             player:set_moon({visible = false})
+            player:set_stars({visible = false})
 			player_list[name] = "redsky"
 			if otherworlds.settings.gravity.enable then
 				player:set_physics_override({gravity = 0.2})
@@ -111,6 +114,7 @@ minetest.register_globalstep(function(dtime)
             })
             player:set_sun({visible = false, sunrise_visible = false})
             player:set_moon({visible = false})
+            player:set_stars({visible = false})
 			player_list[name] = "blackness"
 			if otherworlds.settings.gravity.enable then
 				player:set_physics_override({gravity = 0.1})
