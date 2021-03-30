a09372f0f8a60c5adf4eb85993290573972d33dd
Oversword <bionc:oversword.co.uk>
2021-03-30 02:10:35 +0100

    Fixed sizes and transparencies

--- crystals.lua
+++ crystals.lua
@@ -5,17 +5,18 @@ local sbox = {
 }
 
 local crystal_list = {
-	{"ghost_crystal", "ghost_crystal.png",},
-	{"red_crystal", "red_crystal.png",},
-	{"rose_quartz", "rose_quartz.png",},
+	{"ghost_crystal", "ghost_crystal.png", "Ghost"},
+	{"red_crystal", "red_crystal.png", "Red"},
+	{"rose_quartz", "rose_quartz.png", "Rose"},
 }
 
 for i in ipairs(crystal_list) do
 	local name = crystal_list[i][1]
 	local texture = crystal_list[i][2]
+	local label = crystal_list[i][3]
 
 	minetest.register_node(":crystals:"..name.."_1", {
-		description = "Glowing Crystal",
+		description = "Glowing "..label.." Crystal",
 		drawtype = "mesh",
 		mesh = "crystal_shape01.obj",
 		tiles = {"crystals_"..texture,},
@@ -25,13 +26,13 @@ for i in ipairs(crystal_list) do
 		walkable = false,
 		light_source = 10,
 		use_texture_alpha = true,
-		visual_scale = 10,
+		-- visual_scale = 10,
 		groups = {snappy=2,choppy=2,oddly_breakable_by_hand=3,},
 		sounds = default.node_sound_glass_defaults(),
 	})
 
 	minetest.register_node(":crystals:"..name.."_2", {
-		description = "Glowing Crystal",
+		description = "Glowing "..label.." Crystal",
 		drawtype = "mesh",
 		mesh = "crystal_shape02.obj",
 		tiles = {"crystals_"..texture,},
@@ -41,7 +42,7 @@ for i in ipairs(crystal_list) do
 		walkable = false,
 		light_source = 10,
 		use_texture_alpha = true,
-		visual_scale = 10,
+		-- visual_scale = 10,
 		groups = {snappy=2,choppy=2,oddly_breakable_by_hand=3,},
 		sounds = default.node_sound_glass_defaults(),
 	})
